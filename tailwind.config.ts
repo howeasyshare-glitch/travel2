5a5e0b23ea9c36a153790c6936420ac9837f5a3d
howeasyshare-glitch <howeasyshare@gmail.com>
2026-01-05 11:07:57 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   // 使用更寬鬆的匹配，確保掃描到所有角落
   content: [
+    "./app/api/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}", // 預防萬一你的結構在 src 裡
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
