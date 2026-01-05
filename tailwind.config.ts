3ce3e01812559c36d91cc368f72133cf334995e5
howeasyshare-glitch <howeasyshare@gmail.com>
2026-01-06 00:05:44 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,12 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./page.tsx", // 額外補上一行確保掃描到首頁
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
-export default config;
+}
